1ddd501edc6657941af99dd1de5dbf7606f5e58f
Markus <markussmadsbakken@gmail.com>
2024-02-06 12:56:55 +0100

    finished setup

--- src/test/java/no/ntnu/idatt1002/demo/repo/MyEntityRepoTest.java
+++ src/test/java/no/ntnu/idatt1002/demo/repo/MyEntityRepoTest.java
@@ -1,16 +1,16 @@
 package no.ntnu.idatt1002.demo.repo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import no.ntnu.idatt1002.demo.data.MyEntity;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+class MyEntityRepoTest {
 
-public class MyEntityRepoTest {
-
-    @Test
-    public void testThatWeCanReadMyEntityFromDatabase() {
-        MyEntity e = new MyEntityRepo().getMyEntity("id");
-        assertEquals(e.getName(), "name");
-    }
+  @Test
+  void testThatWeCanReadMyEntityFromDatabase() {
+    MyEntity e = new MyEntityRepo().getMyEntity("id");
+    assertEquals("name", e.getName());
+  }
 
 }
