2783bdd405a9714e38a4c394dc07b2b2b2d9ea50
jonasreiher <jonareih@stud.ntnu.no>
2024-02-13 13:44:22 +0100

    Maxmize screen on application launch

--- src/main/java/no/ntnu/idatt1002/demo/view/App.java
+++ src/main/java/no/ntnu/idatt1002/demo/view/App.java
@@ -5,11 +5,13 @@ import java.util.Properties;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.SubScene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import no.ntnu.idatt1002.demo.ConfigLoader;
 import no.ntnu.idatt1002.demo.Logger;
@@ -29,9 +31,14 @@ public class App extends Application {
 
   @Override
   public void start(Stage primaryStage) throws Exception {
+    Screen screen = Screen.getPrimary();
+    Rectangle2D bounds = screen.getVisualBounds();
+    primaryStage.setX(bounds.getMinX());
+    primaryStage.setY(bounds.getMinY());
+
     primaryStage.setTitle(p.getProperty("app_name"));
     StackPane root = new StackPane();
-    primaryStage.setScene(new Scene(root, 600, 300));
+    primaryStage.setScene(new Scene(root, bounds.getWidth(), bounds.getHeight()));
     primaryStage.show();
   }
 }
