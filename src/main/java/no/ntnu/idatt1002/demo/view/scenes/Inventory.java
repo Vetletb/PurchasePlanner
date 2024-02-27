4604a93b8e76d974b353449efd8991b9937b75ad
Markus <markussmadsbakken@gmail.com>
2024-02-27 13:47:07 +0100

    fixed svg fill and scaling

--- src/main/java/no/ntnu/idatt1002/demo/view/scenes/Inventory.java
+++ src/main/java/no/ntnu/idatt1002/demo/view/scenes/Inventory.java
@@ -2,6 +2,7 @@ package no.ntnu.idatt1002.demo.view.scenes;
 
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import no.ntnu.idatt1002.demo.view.components.Icon;
 
 /**
  * The inventory page.
@@ -12,6 +13,9 @@ public class Inventory extends VBox {
     super();
 
     super.getChildren().addAll(new Text("Inventory"));
+    Icon icon = new Icon("testIcon");
+    icon.setSize(2);
+    super.getChildren().add(icon);
   }
 
 }
