1b3e4661518e14a040bb9011a082e4e13d5ac2a7
florianleucht <fsleucht@stud.ntnu.no>
2024-03-15 12:09:20 +0100

    created added javadoc

--- src/main/java/no/ntnu/idatt1002/demo/data/QuantityItem.java
+++ src/main/java/no/ntnu/idatt1002/demo/data/QuantityItem.java
@@ -3,17 +3,34 @@ package no.ntnu.idatt1002.demo.data;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This class is a simple bean for a quantity item. A quantity item is an item with a quantity and a unit.
+ */
+
 public class QuantityItem extends Item {
   private int quantityItem_id;
   private final int quantity;
   private final String unit;
 
+  /**
+   * Constructor for the QuantityItem class. When creating a new quantity item, it is required that there is an
+   * item in the database with the same name, category and allergy.
+   * @param name the name of the item
+   * @param category the category of the item
+   * @param allergy the allergy of the item
+   * @param quantity the quantity of the item
+   * @param unit the unit of the item
+   */
   public QuantityItem(int item_id, String name, String category, String allergy, int quantity, String unit) {
     super(item_id, name, category, allergy);
     this.quantity = quantity;
     this.unit = unit;
   }
 
+  /**
+   * This method returns the attributes of the quantity item.
+   * @return the attributes of the quantity item
+   */
   @Override
   public List<String> getAttributes() {
     List<String> attributes = new ArrayList<>();
@@ -23,6 +40,10 @@ public class QuantityItem extends Item {
     return attributes;
   }
 
+  /**
+   * This method returns the attribute names of the quantity item.
+   * @return the attribute names of the quantity item
+   */
   @Override
   public List<String> getAttributeNames() {
     List<String> attributes = new ArrayList<>();
@@ -32,20 +53,36 @@ public class QuantityItem extends Item {
     return attributes;
   }
 
+  /**
+   * This method returns the id of the quantity item.
+   * @return the id of the quantity item
+   */
   @Override
   public int getId() {
     return quantityItem_id;
   }
 
+  /**
+   * This method returns the name of the id.
+   * @return the name of the id
+   */
   @Override
   public String getIdName() {
     return "quantityItem_id";
   }
 
+  /**
+   * Getter method for the quantity of the quantity item.
+   * @return the quantity of the quantity item
+   */
   public int getQuantity() {
     return quantity;
   }
 
+  /**
+   * Getter method for the unit of the quantity item.
+   * @return the unit of the quantity item
+   */
   public String getUnit() {
     return unit;
   }
